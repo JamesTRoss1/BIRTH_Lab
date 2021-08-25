3a3a5e8d68ee97f4651636ef072b2f534655ca7f
JamesTRoss1 <78627714+JamesTRoss1@users.noreply.github.com>
2021-08-25 14:16:33 -0700

    Add files via upload

---
+++ MotorRobotConditionals.py
@@ -0,0 +1,37 @@
+#file determines if motor has to activate to correct path errors 
+#Written By James Ross 
+
+#Distance away from needle
+robotDistance = None
+#Distance of rope dispensed 
+motorDistance = None
+tension = None 
+tether = None
+desiredTether = None
+desiredTension = None
+
+def main():
+    #determines how much to move by in given cycle 
+    robot_correction = 1
+    #determines the range of values allowed  
+    robot_precision = 1
+    #determines how much to move by in given cycle 
+    motor_correction = 1
+    #determines the range of values allowed  
+    motor_precision = 1
+    if(desiredTension < tension - robot_precision):
+        robotDistance = robotDistance + robot_correction
+    elif(desiredTension > tension + robot_precision):
+        robotDistance = robotDistance - robot_correction
+    else:
+        pass
+    if(desiredTether < tether - motor_precision):
+        motorDistance = motorDistance - motor_correction
+    elif(desiredTether > tether + motor_precision):
+        motorDistance = motorDistance + motor_correction
+    else:
+        pass 
+    return motorDistance, robotDistance
+
+if __name__ == "__main__":
+    main()
